196bc997dba18b179556b2f2bafe1e9684b6b826
Ali Fadlallah <alifadlallah355@gmail.com>
2024-02-15 23:41:26 +0200

    feat: RadioTab

--- lib/ui/homeScreen/radio/RadioTab.dart
+++ lib/ui/homeScreen/radio/RadioTab.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class RadioTab extends StatelessWidget {
   const RadioTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      color: Colors.lightGreenAccent,
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/img_radio.png'),
+          Text(
+            'إذاعة القرآن الكريم',
+            style: TextStyle(
+                fontFamily: GoogleFonts.elMessiri().fontFamily,
+                fontSize: 25,
+                fontWeight: FontWeight.w400),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              GestureDetector(
+                onTap: () {}, // Image tapped
+                child: Image.asset(
+                  'assets/images/ic_previous.png',
+                  fit: BoxFit.cover, // Fixes border issues
+                ),
+              ),
+              GestureDetector(
+                onTap: () {}, // Image tapped
+                child: Image.asset(
+                  'assets/images/ic_play.png',
+                  fit: BoxFit.cover, // Fixes border issues
+                ),
+              ),
+              GestureDetector(
+                onTap: () {}, // Image tapped
+                child: Image.asset(
+                  'assets/images/ic_next.png',
+                  fit: BoxFit.cover, // Fixes border issues
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
